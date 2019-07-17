a63344a3b3a3cee1d1dce270c7a7f4dad7abfa00
@ZeRonaldo <joseronaldosjr@gmail.com>
2019-07-17 14:10:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('public'));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/index.html'));
+});
+app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/login.html'));
+});
+
+app.get('/register', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/cadastro.html'));
+});
+app.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/home.html'));
+});
+app.get('/user', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/usuario.html'));
+});
+app.get('/event', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/evento.html'));
+});
+app.get('/search', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/busca.html'));
+});
+app.get('/new', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/criarevento.html'));
+});
+
+app.listen(3000);
