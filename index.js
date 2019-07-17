6d0146cdc8da1eef5b3334dab5da351271bc7edc
@ZeRonaldo <joseronaldosjr@gmail.com>
2019-07-17 14:23:39 -0300

    refactor: index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
@@ -31,4 +32,4 @@ app.get('/new', function(req, res) {
     res.sendFile(path.join(__dirname + '/src/criarevento.html'));
 });
 
-app.listen(3000);
+app.listen(port);
